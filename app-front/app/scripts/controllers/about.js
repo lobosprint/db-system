15120fd07f54d8a2c22811fc2097c3b9d880b764
Marcos Antoli <marcosantoli95@gmail.com>
2016-10-12 16:59:10 -0400

    Added app front for angularjs

---
+++ app-front/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appFrontApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the appFrontApp
+ */
+angular.module('appFrontApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app-front/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the appFrontApp
+ */
+angular.module('appFrontApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
