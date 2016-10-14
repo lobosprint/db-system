0995d3fd24d4e3f9dd30ad1f4d7cc7c5f9b7a9c6
Unknown <carlos.fuentes1@upr.edu>
2016-10-14 17:37:27 -0400

    Added Controller interaction with Backend server

--- app-frontend/app/scripts/controllers/adminCurrentTurn.js
+++ app-frontend/app/scripts/controllers/adminCurrentTurn.js
@@ -7,11 +7,29 @@
  * # AdminCurrentTurnCtrl
  * Controller of the appFrontApp
  */
-angular.module('appFrontApp')
-  .controller('AdminCurrentTurnCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+ angular.module('appFrontApp')
+ .controller('AdminCurrentTurnCtrl',['$http','$log','$scope', function ($http, $log,$scope) {
+ 	this.awesomeThings = [
+ 	'HTML5 Boilerplate',
+ 	'AngularJS',
+ 	'Karma'
+ 	];
+ 	
+ 	$scope.testing= "paila";
+ 	$scope.student=[];
+ 	$http.get('/app-backend/getStudent').success(function(data){
+ 			$scope.student=data;
+ 			$log.error(data);
+ 		});
+ 
+ 	
+ 	
+ }]);
+
+ // (function(){
+ // 	var app= angular.module('appFrontApp',[]);
+
+ // 	app.controller('AdminCurrentTurnCtrl',function(){
+ // 	var testing='123444';
+ // 	});
+ // })
