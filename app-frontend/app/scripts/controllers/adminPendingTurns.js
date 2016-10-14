d77b9cab046df86aa733b607b92ef91a62c4525c
Unknown <carlos.fuentes1@upr.edu>
2016-10-14 19:32:04 -0400

    AdminPending Turns feeding from backend

--- app-frontend/app/scripts/controllers/adminPendingTurns.js
+++ app-frontend/app/scripts/controllers/adminPendingTurns.js
@@ -8,10 +8,16 @@
  * Controller of the appFrontApp
  */
 angular.module('appFrontApp')
-  .controller('AdminPendingTurnsCtrl', function () {
+  .controller('AdminPendingTurnsCtrl', ['$http','$log','$scope', function ($http, $log,$scope)  {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+ 	$scope.turns=[];
+ 	$http.get('/app-backend/getTurns').success(function(data){
+ 			$scope.turns=data;
+ 			$log.error(data);
+ 		});
+  }]);
