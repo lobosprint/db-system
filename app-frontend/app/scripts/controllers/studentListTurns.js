67c84ddd3ba6ef93ef555e3e7869392859a0f56a
Unknown <carlos.fuentes1@upr.edu>
2016-10-14 19:07:03 -0400

    Added info from backend to adminCurrentTurn and studentListTurns

--- app-frontend/app/scripts/controllers/studentListTurns.js
+++ app-frontend/app/scripts/controllers/studentListTurns.js
@@ -8,10 +8,17 @@
  * Controller of the appFrontApp
  */
 angular.module('appFrontApp')
-  .controller('StudentListTurnsCtrl', function () {
+  .controller('StudentListTurnsCtrl', ['$http','$log','$scope', function ($http, $log,$scope){
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+
+ 	$scope.turns=[];
+ 	$http.get('/app-backend/getTurns').success(function(data){
+ 			$scope.turns=data;
+ 			$log.error(data);
+ 		});
+  }]);
