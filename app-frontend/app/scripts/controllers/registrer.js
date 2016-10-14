06cd7dd11cd054448903e046f02192314eb90b08
Marcos Antoli <marcosantoli95@gmail.com>
2016-10-14 08:24:11 -0400

    Modified views angularjs

--- app-frontend/app/scripts/controllers/registrer.js
+++ app-frontend/app/scripts/controllers/registrer.js
@@ -10,14 +10,31 @@
 angular.module('appFrontApp')
   .controller('RegistrerCtrl', function ($scope, $http, $log) {
      $scope.signup = function() {
-       var payload = {
-         email : $scope.mail,
-         password : $scope.password
+       var data = {
+            name : $scope.name,
+            middleName : $scope.middleName,
+            lastName : $scope.lastName,
+            rumIdNumber : $scope.rumIdNumber,
+            birthDate : $scope.birthDate,
+            phone : $scope.phone,
+            handicap : $scope.inputHandicap,
+            mail : $scope.mail,
+            pass : $scope.pass,
        };
 
-       $http.post('http://136.145.116.186:9090/registrer', payload)
+       $http.post('http://136.145.116.186:9090/registrer', data)
            .success(function(data) {
              $log.debug(data);
            });
        };
   });
+
+
+
+
+
+
+
+
+
+
