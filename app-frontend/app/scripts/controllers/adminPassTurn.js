c46301f7937fa395a31b13edf6e294be3f5de665
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-11-04 19:46:38 +0000

    fixed error AdminpassTurnCtrl
    AdminPassTurn was not defined correctly in app.js and once it was renamed expressions were loading correctly.

--- app-frontend/app/scripts/controllers/adminPassTurn.js
+++ app-frontend/app/scripts/controllers/adminPassTurn.js
@@ -7,11 +7,20 @@
  * # AdminPassTurnCtrl
  * Controller of the appFrontApp
  */
-angular.module('appFrontApp')
-  .controller('AdminPassTurnCtrl', function () {
+
+  angular.module('appFrontApp')
+  .controller('AdminPassTurnCtrl',['$http','$log','$scope', function ($http, $log, $scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    $scope.turn=[];
+    $http.get('/app-backend/getTurn').success(function(data){
+      $scope.turn=data;
+      //$log.error(data);
+    });
+
+    
+  }]);
