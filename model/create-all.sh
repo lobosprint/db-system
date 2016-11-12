696c12ef98a4e2e84e340b522b08c2381523e9ad
Marcos Antoli <marcosantoli95@gmail.com>
2016-11-12 13:55:05 -0400

    Modified script db

--- model/create-all.sh
+++ model/create-all.sh
@@ -1,5 +1,6 @@
 #sudo -u postgres createuser --superuser turn4u
-psql -h localhost -U postgres -W -f create-user.sql postgres
+#psql -h localhost -U postgres -W -f create-user.sql postgres
+sudo -u postgres dropdb turn4u
 sudo -u postgres createdb turn4u
 psql -h localhost -U turn4u -W -f create-database.sql turn4u
 psql -h localhost -U turn4u -W -f insert-data.sql turn4u
