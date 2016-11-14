5da3b8afc500a602a45de17b9ee3e0e1b43d2e61
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-11-14 03:18:42 +0000

    Updated turn History implementation.
    Turn history now displays DB results for past turn history.

--- app-frontend/app/scripts/controllers/adminTurnsHistory.js
+++ app-frontend/app/scripts/controllers/adminTurnsHistory.js
@@ -1,22 +1,33 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name appFrontApp.controller:AdminTurnsHistoryCtrl
- * @description
- * # AdminTurnsHistoryCtrl
- * Controller of the appFrontApp
- */
+* @ngdoc function
+* @name appFrontApp.controller:AdminTurnsHistoryCtrl
+* @description
+* # AdminTurnsHistoryCtrl
+* Controller of the appFrontApp
+*/
 angular.module('appFrontApp')
-  .controller('AdminTurnsHistoryCtrl', ['$http','$log','$scope', function ($http, $log,$scope) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    $scope.turns=[];
- 	$http.get('/app-backend/getTurns').success(function(data){
- 			$scope.turns=data;
- 			$log.error(data);
- 		});
-  }]);
+.controller('AdminTurnsHistoryCtrl', ['$http','$log','$scope', function ($http, $log,$scope) {
+  this.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Karma'
+  ];
+  $scope.turns=[];
+  $scope.searchedID="";
+  $scope.testString="/app-backend/getAllTurnsByStudentNumber/".concat($scope.searchedID);
+  $scope.getTurnHistory =function(){
+    $http.get("/app-backend/getAllTurnsByStudentNumber/".concat($scope.searchedID)).success(function(data){
+    //var getTurnHistory = $http.get('/app-backend/getTurnsByAdmin/1'.concat($scope.searchedID)).success(function(data){
+    $scope.testString="/app-backend/getAllTurnsByStudentNumber/".concat($scope.searchedID);
+      $scope.turns=data;
+      $log.error(data);
+
+    });
+  }
+  $http.get('/app-backend/getTurns').success(function(data){
+    $scope.turns=data;
+    $log.error(data);
+  });
+}]);
