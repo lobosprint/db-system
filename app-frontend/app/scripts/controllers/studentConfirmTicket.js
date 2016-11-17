1b02f2351e698b7f46fa747d7332b0e2f2d0a663
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-11-17 18:57:51 +0000

    Updated StudentGetTurn and studentConfirmTicket
    studentConfirmTicket  was updated with with dynamic calculation of wait time and queue. 
    
    studentgetturn was updated to display correct admin name and ID of administrative

--- app-frontend/app/scripts/controllers/studentConfirmTicket.js
+++ app-frontend/app/scripts/controllers/studentConfirmTicket.js
@@ -8,10 +8,28 @@
  * Controller of the appFrontApp
  */
 angular.module('appFrontApp')
-  .controller('StudentConfirmTicketCtrl', function () {
+  .controller('StudentConfirmTicketCtrl',['$http','$log','$scope','turnService', function ($http, $log,$scope,turnService) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    //turnVariable is shared between this and studentGetTurn
+    $scope.turnQueue=[];
+    $scope.sharedVariable=turnService.studentGetTurnSharedObject;
+    // $http.get('/app-backend/getWaitQueue/'.concat( $scope.sharedVariable.studentGetTurnAreaID.concat($scope.sharedVariable.studentGetTurnJobID.concat($scope.sharedVariable.studentGetTurnAdminID)))).success(function(data){
+    //   $scope.turnQueue=data;
+    // };
+    $scope.studentConfirmTicketGetRoute="/app-backend/getWaitQueue/";
+    $scope.studentConfirmTicketGetRoute=  $scope.studentConfirmTicketGetRoute.concat($scope.sharedVariable.studentGetTurnAreaID);
+    $scope.studentConfirmTicketGetRoute=  $scope.studentConfirmTicketGetRoute.concat("/");
+    $scope.studentConfirmTicketGetRoute=  $scope.studentConfirmTicketGetRoute.concat($scope.sharedVariable.studentGetTurnJobID);
+    $scope.studentConfirmTicketGetRoute=  $scope.studentConfirmTicketGetRoute.concat("/");
+    $scope.studentConfirmTicketGetRoute=  $scope.studentConfirmTicketGetRoute.concat($scope.sharedVariable.studentGetTurnAdminID);
+    $log.error($scope.studentConfirmTicketGetRoute);
+    $http.get($scope.studentConfirmTicketGetRoute).success(function(data){
+      $scope.turnQueue=data;
+      $log.error($scope.turnQueue.length);
+      //$log.error(data);
+    });
+  }]);
