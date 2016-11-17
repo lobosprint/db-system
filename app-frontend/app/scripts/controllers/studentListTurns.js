703eb38a482664057bef8f1190314b6d84c8d560
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-11-17 23:22:46 +0000

    Updated studentListTurns for request perID

--- app-frontend/app/scripts/controllers/studentListTurns.js
+++ app-frontend/app/scripts/controllers/studentListTurns.js
@@ -1,23 +1,26 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name appFrontAPendingontroller:StudentListTurnsCtrl
- * @description
- * # StudentListTurnsCtrl
- * Controller of the appFrontApp
- */
+* @ngdoc function
+* @name appFrontAPendingontroller:StudentListTurnsCtrl
+* @description
+* # StudentListTurnsCtrl
+* Controller of the appFrontApp
+*/
 angular.module('appFrontApp')
-  .controller('StudentListTurnsCtrl', ['$http','$log','$scope', function ($http, $log,$scope){
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+.controller('StudentListTurnsCtrl', ['$http','$log','$scope', function ($http, $log,$scope){
 
- 	$scope.turns=[];
- 	$http.get('/app-backend/getTurnsByStudent/1').success(function(data){
- 			$scope.turns=data;
- 			$log.error(data);
- 		});
-  }]);
+  $scope.ID=1;
+  $scope.turns=[];
+  $http.get('/app-backend/getTurnsByStudent/1').success(function(data){
+    $scope.turns=data;
+  });
+  $scope.getPendingTurns=function(){
+
+
+    $http.get('/app-backend/getTurnsByStudent/'.concat($scope.ID)).success(function(data){
+      $scope.turns=data;
+      $log.error(data);
+    });
+  }
+}]);
