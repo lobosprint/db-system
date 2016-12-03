2c092988e8766fec60802e6076b2b3ff8c6a5f59
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-12-03 21:22:43 +0000

    Prepared controller for http request.

--- app-frontend/app/scripts/controllers/studentPayPenalty.js
+++ app-frontend/app/scripts/controllers/studentPayPenalty.js
@@ -7,11 +7,18 @@
  * # StudentPayPenaltyCtrl
  * Controller of the appFrontApp
  */
-angular.module('appFrontApp')
-  .controller('StudentPayPenaltyCtrl', function () {
+
+  angular.module('appFrontApp')
+  .controller('StudentPayPenaltyCtrl', ['$http','$log','$scope', 'turnService', function ($http, $log,$scope, turnService)  {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+    $scope.turnVariable=turnService.sharedObject;
+    $scope.turns=[];
+    // $http.get('/app-backend/getTurnsByAdmin/1').success(function(data){
+    //   $scope.turns=data;
+    //   //$log.error(data);
+    // });
+  }]);
