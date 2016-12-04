0f45983d48d6a8592be933ca3157cb08d754368a
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-12-04 18:25:05 +0000

    Updated studentListTurns with cookie

--- app-frontend/app/scripts/controllers/studentListTurns.js
+++ app-frontend/app/scripts/controllers/studentListTurns.js
@@ -10,17 +10,10 @@
 angular.module('appFrontApp')
 .controller('StudentListTurnsCtrl', ['$http','$log','$scope', function ($http, $log,$scope){
 
-  $scope.ID=1;
+
   $scope.turns=[];
-  $http.get('/app-backend/getTurnsByStudent/1').success(function(data){
+  $http.get('/app-backend/getTurnsByStudent/'.concat(localStorage.getItem('id'))).success(function(data){
     $scope.turns=data;
   });
-  $scope.getPendingTurns=function(){
-
-
-    $http.get('/app-backend/getTurnsByStudent/'.concat($scope.ID)).success(function(data){
-      $scope.turns=data;
-      $log.error(data);
-    });
-  }
+    
 }]);
