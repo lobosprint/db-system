ac85879053f94aafe0b58b16238beb5b1fdde3dd
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-12-04 17:25:02 +0000

    Updated login screen validation and prepared post
    Post method needs to be updated once it is ready on backend.

--- app-frontend/app/scripts/controllers/main.js
+++ app-frontend/app/scripts/controllers/main.js
@@ -1,25 +1,47 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name appFrontApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the appFrontApp
- */
+* @ngdoc function
+* @name appFrontApp.controller:MainCtrl
+* @description
+* # MainCtrl
+* Controller of the appFrontApp
+*/
 angular.module('appFrontApp')
-  .controller('MainCtrl', function ($scope, $http, $log) {
-     $scope.signup = function() {
-       var values = {
-         mail : $scope.mail,
-         pass : $scope.pass
-       };
+.controller('MainCtrl', ['$scope','$http','$log','$location','$window',function ($scope, $http, $log, $location, $window) {
 
-       $log.error(values);
+  $scope.mail="";
+  $scope.pass="";
+  $scope.signup = function() {
+    if($scope.mail==""|| $scope.pass==""){
+      alert("Hay campos que le falta por completar, favor revisar campos con *");
+    }
+    else{
+      var values = {
+        mail : $scope.mail,
+        pass : $scope.pass
+      };
 
-       $http.post('http://136.145.216.150:9090/app-backend/login', values)
-           .success(function(data) {
-             $log.debug(data);
-           });
-       };
-  });
+      //$log.error(values);
+
+      $http.post('/app-backend/login', values)
+      .success(function(data) {
+        //   localStorage.setItem('user', 'nombre');
+        //   localStorage.setItem('role', 'tipo-de-rol');
+        //   localStorage.setItem('id', 'id-persona');
+        // //Generate If Statement depending on tipo-de-rol
+        //   window.location.replace("/#/studentHome");
+        //   window.location.replace("/#/adminHome");
+
+
+        //$log.debug(data);
+      }).error(function(data){
+        // alert(data);
+        $('.panel.panel-success').addClass('panel-danger');
+        $('.panel.panel-success').removeClass('panel-success');
+        $('.panel-heading').text("Log in - No puedes inicar sesión con esas credenciales");
+
+      });
+    }
+  };
+}]);
