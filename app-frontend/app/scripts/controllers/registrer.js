d2933027fe061113a279b073ae2ce98de2a61f33
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-12-04 00:57:15 +0000

    Added registerUser Post Method and in HTML

--- app-frontend/app/scripts/controllers/registrer.js
+++ app-frontend/app/scripts/controllers/registrer.js
@@ -1,40 +1,51 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name appFrontApp.controller:RegistrerCtrl
- * @description
- * # RegistrerCtrl
- * Controller of the appFrontApp
- */
+* @ngdoc function
+* @name appFrontApp.controller:RegistrerCtrl
+* @description
+* # RegistrerCtrl
+* Controller of the appFrontApp
+*/
 angular.module('appFrontApp')
-  .controller('RegistrerCtrl', function ($scope, $http, $log) {
-     $scope.signup = function() {
-       var data = {
-            name : $scope.name,
-            middleName : $scope.middleName,
-            lastName : $scope.lastName,
-            rumIdNumber : $scope.rumIdNumber,
-            birthDate : $scope.birthDate,
-            phone : $scope.phone,
-            handicap : $scope.inputHandicap,
-            mail : $scope.mail,
-            pass : $scope.pass,
-       };
-
-       $http.post('http://136.145.116.186:9090/registrer', data)
-           .success(function(data) {
-             $log.debug(data);
-           });
-       };
-  });
+.controller('RegistrerCtrl', ['$scope', '$http', '$log', 'turnService',function ($scope, $http, $log, turnService) {
+  //  $scope.signup = function() {
+  //    var data = {
+  //         name : $scope.name,
+  //         middleName : $scope.middleName,
+  //         lastName : $scope.lastName,
+  //         rumIdNumber : $scope.rumIdNumber,
+  //         birthDate : $scope.birthDate,
+  //         phone : $scope.phone,
+  //         handicap : $scope.inputHandicap,
+  //         mail : $scope.mail,
+  //         pass : $scope.pass,
+  //    };
 
 
+  //};
+  $scope.sharedVariable=turnService.studentRegisterSharedobject;
+  $scope.sharedVariable.handiecap=false;
+  $scope.sharedVariable.first_name="";
+  $scope.sharedVariable.middle_name="";
+  $scope.sharedVariable.last_name="";
+  $scope.sharedVariable.date_birth="";
+  $scope.sharedVariable.phone="";
+  $scope.sharedVariable.email="";
+  $scope.sharedVariable.password="";
+  $scope.sharedVariable.rum_id="";
+
+  $scope.config = {
+    headers : {
+      'Content-Type': 'application/json'
+    }
+  }
 
 
+  $scope.registerUser= function(){
+    $http.post('/app-backend/addStudent',$scope.sharedVariable,$scope.config).success(function(data){
+      $log.error(data);
+    });
 
-
-
-
-
-
+  };
+}]);
