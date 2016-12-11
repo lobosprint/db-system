287612d8176caf4e19d3b42acd149498c0151ae7
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-12-11 08:07:37 +0000

    Updated Pending Turns for admin with dynamic jobs(needs bug fix)
    Waiting on backend for confirmation.

--- app-frontend/app/scripts/controllers/adminPendingTurns.js
+++ app-frontend/app/scripts/controllers/adminPendingTurns.js
@@ -9,15 +9,48 @@
 */
 angular.module('appFrontApp')
 .controller('AdminPendingTurnsCtrl', ['$http','$log','$scope', 'turnService', function ($http, $log,$scope, turnService)  {
-  this.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Karma'
-  ];
+
+  $scope.job=-1;
+  $scope.jobs=[];
+  $scope.secretario=false;;
+  $scope.profesor=false;
+  $scope.consejero=false;
+
+  $http.get('/app-backend/getJobsofTurnsAdmin/'.concat(localStorage.getItem('id_administrative'))).success(function(data){
+    $scope.jobs=data;
+    for( var i =0; i<$scope.jobs.length; i++){
+      if ($scope.secretario==false && $scope.jobs[i].idJob==1){
+        $scope.secretario=true;
+      }
+      if($scope.profesor==false && $scope.jobs[i].idJob==2){
+        $scope.profesor=true;
+      }
+      if($scope.consejero=false && $scope.jobs[i].idJob==3){
+        $scope.consejero=true;
+      }
+    }
+  });
   $scope.turnVariable=turnService.sharedObject;
   $scope.turns=[];
-  $http.get('/app-backend/getTurnsByAdmin/'.concat(localStorage.getItem('id'))).success(function(data){
+  $http.get('/app-backend/getTurnsByAdmin/'.concat(localStorage.getItem('id_administrative'))).success(function(data){
     $scope.turns=data;
     //$log.error(data);
   });
+$scope.getAllTurnsByAdmin= function(){
+  $http.get('/app-backend/getTurnsByAdmin/'.concat(localStorage.getItem('id_administrative'))).success(function(data){
+    $scope.turns=data;
+    //$log.error(data);
+  });
+};
+  $log.error('/app-backend/getTurnsByAdminJob/'.concat(localStorage.getItem('id_administrative')).concat('/').concat($scope.job));
+$scope.getActiveTurns= function(){
+  $log.error("In Function");
+  $log.error('/app-backend/getTurnsByAdminJob/'.concat(localStorage.getItem('id_administrative')).concat('/').concat($scope.job));
+
+  $http.get('/app-backend/getTurnsByAdminJob/'.concat(localStorage.getItem('id_administrative')).concat('/').concat($scope.job)).success(function(data){
+    $scope.turns=data;
+    $log.error(data);
+  });
+} ;
+
 }]);
