3d6bd1328d38d6093de1e24ce76c795062fa1288
Carlos Fuentes <carlos.fuentes1@upr.edu>
2016-12-12 07:24:16 +0000

    Delete turn in studentListTurns Completed

--- app-frontend/app/scripts/controllers/studentListTurns.js
+++ app-frontend/app/scripts/controllers/studentListTurns.js
@@ -10,10 +10,31 @@
 angular.module('appFrontApp')
 .controller('StudentListTurnsCtrl', ['$http','$log','$scope', function ($http, $log,$scope){
 
-
+  $scope.currentID={
+    id_turn:0
+  }
   $scope.turns=[];
   $http.get('/app-backend/getTurnsByStudent/'.concat(localStorage.getItem('id_student'))).success(function(data){
     $scope.turns=data;
+    $log.error('test');
   });
+  $scope.testing= function (){
+    if(confirm("testing")==true){
+      $http.post('/app-backend/deleteTurn',$scope.currentID).success(function(data){
+        $scope.turns=[];
+        $log.error('turn Deleted');
+        $http.get('/app-backend/getTurnsByStudent/'.concat(localStorage.getItem('id_student'))).success(function(data){
+          $scope.turns=data;
+    
+        });
+      });
+
+      $log.error('true: '+$scope.currentID.id_turn);
+    }
+    else {
+      $log.error('cancelled');
+    }
+
+  };
 
 }]);
